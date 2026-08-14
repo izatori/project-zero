856a442d1e8db08cfe74bd6120b45d40e6d3ef63
David vG <naninoiza@outlook.com>
2026-08-14 18:06:47 +0200

    added basic structure for product repository implementation

---
+++ ProjectZero/Core.Infrastructure/Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,39 @@
+﻿using Core.Domain.Entities;
+using Core.Domain.Repositories;
+
+namespace Core.Infrastructure.Persistence.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    private readonly ApplicationDbContext _dbContext;
+    
+    public ProductRepository(ApplicationDbContext context)
+    {
+        _dbContext = context ??  throw new ArgumentNullException(nameof(context));
+    }
+    
+    public Task AddAsync(Product aggregate, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task UpdateAsync(Product aggregate, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteAsync(Product aggregate, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Product?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Product>> GetAllActiveAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+}
