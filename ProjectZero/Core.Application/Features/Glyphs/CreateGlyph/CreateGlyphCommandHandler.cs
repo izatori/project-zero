ffc63497d46e8fb3b96ea584172bc1da3190ccc2
izatori <naninoiza@outlook.com>
2026-08-15 22:10:24 +0200

    implemented the handler for the create-glyph-command

---
+++ ProjectZero/Core.Application/Features/Glyphs/CreateGlyph/CreateGlyphCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Core.Domain.Abstractions;
+using Core.Domain.Entities;
+using Core.Domain.Repositories;
+using MediatR;
+
+namespace Core.Application.Features.Glyphs.CreateGlyph;
+
+public class CreateGlyphCommandHandler : IRequestHandler<CreateGlyphCommand, Guid>
+{
+    private readonly IGlyphRepository _glyphRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CreateGlyphCommandHandler(IGlyphRepository glyphRepository, IUnitOfWork unitOfWork)
+    {
+        _glyphRepository = glyphRepository ?? throw new ArgumentNullException(nameof(glyphRepository));
+        _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+    }
+
+    public async Task<Guid> Handle(CreateGlyphCommand command, CancellationToken cancellationToken = default)
+    {
+        var glyph = Glyph.Create(
+            command.Character,
+            command.Romaji,
+            command.GlyphType,
+            command.ImageFileName,
+            command.StrokeAnimationFileName);
+
+        await _glyphRepository.AddAsync(glyph, cancellationToken);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return glyph.Id;
+    }
+}
