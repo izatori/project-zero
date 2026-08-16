465bce205c00b30b496b7f90577d57eecff5acdf
izatori <naninoiza@outlook.com>
2026-08-17 00:07:36 +0200

    added some styling for the new learning application and added more images for translations

--- ProjectZero/ProjectZero/wwwroot/resize.js
+++ ProjectZero/ProjectZero/wwwroot/resize.js
@@ -17,11 +17,12 @@ window.fitTextToWidth = function (element, maxSize, minSize) {
     if (!element) return;
     const max = maxSize || 128;
     const min = minSize || 16;
+    const containerWidth = element.parentElement ? element.parentElement.clientWidth : element.clientWidth;
+    const targetWidth = containerWidth * 0.8; // leave 10% margin on each side
     let size = max;
     element.style.fontSize = size + 'px';
     element.style.whiteSpace = 'nowrap';
-    const containerWidth = element.parentElement ? element.parentElement.clientWidth : element.clientWidth;
-    while (element.scrollWidth > containerWidth && size > min) {
+    while (element.scrollWidth > targetWidth && size > min) {
         size -= 1;
         element.style.fontSize = size + 'px';
     }
